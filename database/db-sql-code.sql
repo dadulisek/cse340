9a27e6c5930ed786ef627c584cd3a19591d7bb78
dadulisek <navratildada@gmail.com>
2025-03-14 13:56:05 -0600

    week 2 learning

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -224,3 +224,13 @@ VALUES   (
     'White',
     5
   );
+
+-- Updating `description` on GM Hummer
+UPDATE public.inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_id = 10;
+
+-- Corecting file path in `inventory`
+UPDATE public.inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+	inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
