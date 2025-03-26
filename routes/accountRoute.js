c11991b9b5189b807d7530473016b789610cfa7f
dadulisek <navratildada@gmail.com>
2025-03-26 17:23:47 -0600

    week 04 learnig activites done

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,10 +3,16 @@ const express = require("express")
 const router = new express.Router() 
 const utilities = require("../utilities/index")
 const accountController = require("../controllers/accountController")
+const regValidate = require('../utilities/account-validation')
 //Routes to do stuff
 router.get("/login", utilities.handleErrors(accountController.buildLogin))
 router.get("/register", utilities.handleErrors(accountController.buildRegister))
-// register account
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
+// Process the registration data
+router.post(
+    "/register",
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+  )
 
 module.exports = router;
