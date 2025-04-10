0df043399f325c2286fc58f30025abb7a0022bf6
dadulisek <navratildada@gmail.com>
2025-04-10 12:24:14 +0200

    week 05 assingment first half

--- utilities/account-validation.js
+++ utilities/account-validation.js
@@ -74,5 +74,63 @@ validate.checkRegData = async (req, res, next) => {
     }
     next()
   }
+
+  /*  **********************************
+*  Login Data Validation Rules
+* ********************************* */
+validate.loginRules = () => {
+  console.log("loginRules Zacatek")
+  return [
+    // valid email is required and cannot already exist in the DB
+    body("account_email")
+      .trim()
+      .escape()
+      .notEmpty()
+      .isEmail()
+      .normalizeEmail() // refer to validator.js docs
+      .withMessage("A valid email is required."),
+      // .custom(async (account_email) => {
+      //     const emailExists = await accountModel.checkExistingEmail(account_email)
+      //     if (emailExists&&!emailExists.rowCount){
+      //       throw new Error("Email not exists. Please register your information through sign-up link below.")
+      //     }
+      // }),
+
+    // password is required and must be strong password
+    body("account_password")
+      .trim()
+      .notEmpty()
+      .isStrongPassword({
+        minLength: 12,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1,
+      })
+      .withMessage("Password does not meet requirements."),
+  ]
+}
+
+/* ******************************
+* Check data and return errors or continue to login
+* ***************************** */
+validate.checkLoginData = async (req, res, next) => {
+  console.log("checkLoginData Zacatek")
+  const { account_email } = req.body
+  let errors = []
+  errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    console.log("checkLoginData Porucha")
+    let nav = await utilities.getNav()
+    res.render("account/login", {
+      errors,
+      title: "Login",
+      nav,
+      account_email,
+    })
+    return
+  }
+  next()
+}
   
   module.exports = validate
